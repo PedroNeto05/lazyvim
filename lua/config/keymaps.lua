60bd3c34ed17ee766ccd812da5f1febe2d280fae
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-08-10 16:47:26 -0300

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,14 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local wk = require("which-key")
+local function map(mode, lhs, rhs)
+  local opts = { noremap = true, silent = true }
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+map("n", "<C-a>", "gg<S-v>G")
+wk.add({
+  {
+    { "<leader>a", group = "+ai" },
+  },
+})
