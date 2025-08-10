6278679d2682837555928be6affde0a114f8908c
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2025-08-10 17:03:23 -0300

    lsp group

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,14 +2,26 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local wk = require("which-key")
+
 local function map(mode, lhs, rhs)
   local opts = { noremap = true, silent = true }
   vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
 end
+
 map("n", "<C-a>", "gg<S-v>G")
+
 wk.add({
-  {
-    { "<leader>a", group = "+ai" },
-    { "<leader>cw", require("telescope.builtin").diagnostics, desc = "Diagnostics" },
-  },
+  { "<leader>a", group = "+ai" },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    wk.add({
+      {
+        "<leader>cw",
+        require("telescope.builtin").diagnostics,
+        desc = "Diagnostics",
+      },
+    }, { buffer = args.buf })
+  end,
 })
