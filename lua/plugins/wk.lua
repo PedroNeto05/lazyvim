f71c639c5775ed4e6e8c667acaca2b53606a12cf
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-03-04 10:53:36 -0300

    oil nvim

--- lua/plugins/wk.lua
+++ lua/plugins/wk.lua
@@ -1,6 +1,13 @@
 local ok, wk = pcall(require, "which-key")
 if ok then
   wk.add({
+    {
+      "<leader>E",
+      function()
+        require("oil").open_float()
+      end,
+      desc = "Explorer (Oil Float)",
+    },
     { "<leader>a", group = "+ai" },
   })
 end
