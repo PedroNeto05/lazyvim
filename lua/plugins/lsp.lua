40cd93801cb8f540247c0d100c91174c722dacbe
Pedro Neto <pedro.neto.704@ufrn.edu.br>
2026-04-01 19:26:38 -0300

    disable gopls docs

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -15,6 +15,11 @@ return {
                 parameterNames = false,
                 rangeVariableTypes = false,
               },
+              analyses = {
+                ST1000 = false, -- package docs
+                ST1020 = false, -- exported funcs
+                ST1021 = false, -- exported types
+              },
             },
           },
         },
